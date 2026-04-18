26d0ddd03e8446e74278b8722b5498c7b740b082
kyj <yunju_kim@icloud.com>
2026-04-18 15:31:25 +0900

    검색 함수 적용

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,9 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
         process::exit(1);
     });
     println!("{}에서 {}을 검색합니다.", config.file_path, config.query);
     if let Err(e) = minigrep::run(config) {
+        println!("{e}");
         process::exit(1);
     }
 }
