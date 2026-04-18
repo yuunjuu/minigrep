25db436320624e347af143b0bc2ae6e7e0620715
kyj <yunju_kim@icloud.com>
2026-04-18 14:28:57 +0900

    커맨드 라인 인수 및 검색 내용 출력

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("{}에서 {}을 검색합니다.", file_path, query);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("\n내용: \n{contents}");
 }
