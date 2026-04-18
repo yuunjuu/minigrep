8a871e6c4aed057a2700ecdf19d380d59a60d87b
kyj <yunju_kim@icloud.com>
2026-04-18 14:58:01 +0900

    인수 파싱 에러 처리

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("인수를 파싱하는 중 에러가 발생했습니다. {err}");
+        process::exit(1);
+    });
     println!("{}에서 {}을 검색합니다.", config.file_path, config.query);
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("\n내용: \n{contents}");
@@ -15,9 +19,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("검색을 위한 인수가 충분하지 않습니다.");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
