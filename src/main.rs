14341c1d47c04327cb142dcf2dc281f1d06042cd
kyj <yunju_kim@icloud.com>
2026-04-18 15:27:44 +0900

    검색 함수에 대한 테스트코드 작성

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,10 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("인수를 파싱하는 중 에러가 발생했습니다. {err}");
         process::exit(1);
     });
     println!("{}에서 {}을 검색합니다.", config.file_path, config.query);
     if let Err(e) = minigrep::run(config) {
-        println!("실행 중 에러가 발생했습니다. {e}");
         process::exit(1);
     }
 }
