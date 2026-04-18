6284dfef6141f18ec76947ef3204da3dd496e345
kyj <yunju_kim@icloud.com>
2026-04-18 15:09:33 +0900

    라이브러리 크레이트로 파일 쪼개기

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,30 +10,8 @@ fn main() {
         process::exit(1);
     });
     println!("{}에서 {}을 검색합니다.", config.file_path, config.query);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("실행 중 에러가 발생했습니다. {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("검색을 위한 인수가 충분하지 않습니다.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("\n내용: \n{contents}");
-    Ok(())
 }
