cd68ccf07d340179875388148a8c41b35b2c7dcb
kyj <yunju_kim@icloud.com>
2026-04-18 15:52:17 +0900

    에러 메시지 표준에러로 작성

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("{err}");
+        eprintln!("{err}");
         process::exit(1);
     });
     println!("{}에서 {}을 검색합니다.", config.file_path, config.query);
     if let Err(e) = minigrep::run(config) {
-        println!("{e}");
+        eprintln!("{e}");
         process::exit(1);
     }
 }
